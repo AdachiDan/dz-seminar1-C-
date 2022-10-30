eac4c94f7499ded9b4e4d4eb9a6573dcb45d6478
Daniil T <daniilteremovskiy@gmail.com>
2022-10-30 20:34:28 +0300

    feat: new seminar 2, exmp 14

---
+++ example14(2)/Program.cs
@@ -0,0 +1,26 @@
+﻿void result(int x)
+{
+int ost1 = x % 7;
+int ost2 = x % 23;
+    if (ost1 == 0 && ost2 == 0)
+    {
+        Console.WriteLine("Число " + x + " кратно 23 и 7");
+    }
+    else
+    {
+        Console.WriteLine("Число " + x + " некратно 23 и 7");
+    }
+
+}
+
+Console.WriteLine("Проверка: кратно ли число 7 и 23 одновременно");
+Console.WriteLine("Введите число:");
+
+int x;
+
+while (!int.TryParse(Console.ReadLine()!, out x)){
+   Console.WriteLine("Неверный ввод");
+   Console.WriteLine("Введите число:");
+}
+
+result(x);
