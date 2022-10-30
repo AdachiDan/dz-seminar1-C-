d960a9bd13a9bb44241b146af4999952c130eccf
Daniil T <daniilteremovskiy@gmail.com>
2022-10-30 23:26:17 +0300

    feat: new dz.sem 2, exmp 12

---
+++ dz.zad13(2)/Program.cs
@@ -0,0 +1,29 @@
+﻿void FindNumber(int x)
+{
+int number = x;
+if(number > 99)
+{
+    int digitIndex = 2;
+    int digit = number.ToString()[digitIndex] - '0';
+    Console.WriteLine("Третья цифра: " + digit);
+
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+}
+
+Console.WriteLine("Нахождение третьей цифры числа");
+Console.WriteLine("Введите число:");
+
+int x;
+
+while (!int.TryParse(Console.ReadLine()!, out x)){
+   Console.WriteLine("Неверный ввод");
+   Console.WriteLine("Введите число:");
+}
+
+FindNumber(x);
+
