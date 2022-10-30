304c3b82d48cb6e35fb5086f7918f81304cb0726
Daniil T <daniilteremovskiy@gmail.com>
2022-10-30 19:54:35 +0300

    feat: new seminar 2, exmp 12

---
+++ example12(2)/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Проверка: второе число кратно первому?");
+Console.WriteLine("Введите первое число:");
+string inputString = Console.ReadLine()!;
+int x = int.Parse(inputString);
+
+Console.WriteLine("Введите второе число:");
+string inputString2 = Console.ReadLine()!;
+int x2 = int.Parse(inputString2);
+
+void result(int x, int x2)
+{
+
+    int x3 = x % x2;
+
+    if (x3 == 0)
+    {
+        Console.WriteLine("Число " + x + " кратно " + x2);
+    }
+    else
+    {
+        Console.WriteLine("Число " + x + " некратно " + x2 + " остаток " + x3);
+    }
+
+}
+
+result(x, x2);
