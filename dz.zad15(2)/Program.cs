e9a48b885435f38d7df9dfd56fe665a215b285ec
Daniil T <daniilteremovskiy@gmail.com>
2022-10-30 23:52:39 +0300

    feat: new dz.sem 2, exmp 15

---
+++ dz.zad15(2)/Program.cs
@@ -0,0 +1,33 @@
+﻿void Weekend(int x)
+{
+    if (x == 6 || x == 7)
+    {
+        Console.WriteLine("Выходной (Да)");
+    }
+    else
+    {
+       if((0<x)&&(x<6))
+       {
+        Console.WriteLine("Не выходной (Нет)");
+       }
+       else
+       {
+        Console.WriteLine("Введите число от 1 до 7!");
+       }
+        
+    }
+
+}
+
+Console.WriteLine("Проверка на выходной");
+Console.WriteLine("Введите число от 1 до 7:");
+
+int x;
+
+while (!int.TryParse(Console.ReadLine()!, out x))
+{
+    Console.WriteLine("Неверный ввод");
+    Console.WriteLine("Введите число от 1 до 7:");
+}
+
+Weekend(x);
