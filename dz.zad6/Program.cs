26a146163eeebc3ca669e4112679fdb94b731151
Daniil T <daniilteremovskiy@gmail.com>
2022-10-30 16:51:50 +0300

    feat: delete clear 3

--- dz.zad6/Program.cs
+++ dz.zad6/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Введите число:");
 string number = Console.ReadLine()!;
 int x = int.Parse(number);
-Console.Clear();
+
 if (x % 2 == 0)
 {
     Console.WriteLine("Число " + x + " четное");
