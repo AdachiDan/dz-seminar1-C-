e3ae04d0a7ed69af2026c9c245b3e83e1fd75b84
Daniil T <daniilteremovskiy@gmail.com>
2022-10-30 22:24:06 +0300

    feat: new seminar 2, exmp 16

---
+++ example16(2)/Program.cs
@@ -0,0 +1,37 @@
+﻿bool check(int x, int x2)
+{
+    return x*x == x2 || x2*x2 == x;
+}
+
+void result(int x, int x2)
+{
+    
+    if (check(x, x2))
+    {
+        Console.WriteLine("Является");
+    }
+    else
+    {
+        Console.WriteLine("Не является");
+    }
+
+}
+
+Console.WriteLine("Проверка: является ли одно число квадратом другого?");
+Console.WriteLine("Введите первое число:");
+
+int x;
+
+while (!int.TryParse(Console.ReadLine()!, out x)){
+   Console.WriteLine("Неверный ввод");
+   Console.WriteLine("Введите первое число:");
+}
+Console.WriteLine("Введите второе число:");
+
+int x2;
+
+while (!int.TryParse(Console.ReadLine()!, out x2)){
+   Console.WriteLine("Неверный ввод");
+   Console.WriteLine("Введите второе число:");
+}
+result(x, x2);
