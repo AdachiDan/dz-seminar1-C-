33bb3154684e2d4e2a0f33c9f68423225d5ff459
Daniil T <daniilteremovskiy@gmail.com>
2022-11-06 14:06:29 +0300

    feat: new dz.sem 2, exmp 13

--- dz.zad13(2)/Program.cs
+++ dz.zad13(2)/Program.cs
@@ -1,18 +1,69 @@
-﻿void FindDigit(int x)
+﻿ void FindDigit(int x)
 {
-int number = x;
-if(number > 99)
-{
-    int digitIndex = 2;
-    int digit = number.ToString()[digitIndex] - '0';
-    Console.WriteLine("Третья цифра: " + digit);
-
-}
-else
-{
-    Console.WriteLine("Третьей цифры нет");
-}
-
+    int digit = x;
+    int y = 100;
+    while (digit > 0)
+    {
+        if (digit < y)
+        {
+            Console.WriteLine("Третьей цифры нет");
+            break;
+        }
+        else
+        {
+            y = y * 10;
+            if (digit < y)
+            {
+                int thirdDigit = digit % 10;
+                Console.WriteLine("Третья цифра: " + thirdDigit);
+                break;
+            }
+            else
+            {
+                y = y * 10;
+                if (digit < y)
+                {
+                    digit = digit / 10;
+                    int thirdDigit = digit % 10;
+                    Console.WriteLine("Третья цифра: " + thirdDigit);
+                    break;
+                }
+                else
+                {
+                    y = y * 10;
+                    if (digit < y)
+                    {
+                        digit = digit / 100;
+                        int thirdDigit = digit % 10;
+                        Console.WriteLine("Третья цифра: " + thirdDigit);
+                        break;
+                    }
+                    else
+                    {
+                        y = y * 10;
+                        if (digit < y)
+                        {
+                            digit = digit / 1000;
+                            int thirdDigit = digit % 10;
+                            Console.WriteLine("Третья цифра: " + thirdDigit);
+                            break;
+                        }
+                        else
+                        {
+                             y = y * 10;
+                             if (digit < y)
+                             {
+                               digit = digit / 10000;
+                               int thirdDigit = digit % 10;
+                               Console.WriteLine("Третья цифра: " + thirdDigit);
+                               break;
+                             }
+                        }    
+                    }
+                }
+            }
+        }
+    }
 }
 
 Console.WriteLine("Нахождение третьей цифры числа");
@@ -20,10 +71,13 @@ Console.WriteLine("Введите число:");
 
 int x;
 
-while (!int.TryParse(Console.ReadLine()!, out x)){
-   Console.WriteLine("Неверный ввод");
-   Console.WriteLine("Введите число:");
+while (!int.TryParse(Console.ReadLine()!, out x))
+{
+    Console.WriteLine("Неверный ввод");
+    Console.WriteLine("Введите число:");
 }
 
 FindDigit(x);
+
+
 
