652dc05203bd71c2bc9a03b2e52ab65a48acb931
Daniil T <daniilteremovskiy@gmail.com>
2022-11-06 14:07:29 +0300

    feat: new dz.sem 2, exmp 10

--- dz.zad10(2)/Program.cs
+++ dz.zad10(2)/Program.cs
@@ -1,8 +1,10 @@
-﻿void GetSecondDigit(int x)
+﻿int GetSecondDigit(int x)
 {
     int desyatki = x / 10;
     int iskomoe = desyatki % 10;  
-    Console.WriteLine("Вторая цифра: " + iskomoe);
+   // Console.WriteLine("Вторая цифра: " + iskomoe);
+    x = iskomoe;
+    return (x);
 }
 
 Console.WriteLine("Нахождение второй цифры трехзначного числа");
@@ -15,4 +17,5 @@ while (!int.TryParse(Console.ReadLine()!, out x)){
    Console.WriteLine("Введите трехзначное число:");
 }
 
-GetSecondDigit(x);
+//GetSecondDigit(x);
+Console.WriteLine("Вторая цифра: " + GetSecondDigit(x));
